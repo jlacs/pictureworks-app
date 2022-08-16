2ac63c5498d4aa366f0ad5dd0f624526f2ed8337
jlacs <joselacsoniii@gmail.com>
2022-08-16 21:33:33 +0800

    Setup Controllers, Model, Routes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,9 +12,9 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        return 'test';
     }
 
     /**
